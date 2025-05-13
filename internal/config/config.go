44f1729696970d02b19f12d4ce6c08b5d4704f4c
agermel <agermel@mails.ccnu.edu.cn>
2025-05-13 08:22:58 +0800

    feat(init): 配置文件更新

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,35 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+
 	Auth struct {
 		AccessSecret string
 		AccessExpire int64
 	}
-}
 
+	Log struct {
+		File    string
+		Level   string
+		Maxsize int
+		MaxAge  int
+	}
+
+	MySQL struct {
+		Name     string
+		User     string
+		Password string
+		Host     string
+		Port     int
+	}
+
+	Redis struct {
+		Host     string
+		Port     int
+		Password string
+	}
+}
