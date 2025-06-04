d9e735bae8cb41e92e8013c14a37d0800d349d3f
agermel <agermel@mails.ccnu.edu.cn>
2025-06-04 16:15:33 +0800

    🐛 传日期的时间操作

--- internal/logic/getavailabledayslogic.go
+++ internal/logic/getavailabledayslogic.go
@@ -27,6 +27,8 @@ func NewGetAvailableDaysLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *GetAvailableDaysLogic) GetAvailableDays(Type string) (resp *types.AvailableDatesRes, err error) {
+	debug := l.ctx.Value("DEBUG_MODE")
+
 	//获取可用天数
 	dates, err := l.svcCtx.SeatModel.GetAvaliabledays(l.ctx)
 	if err != nil {
@@ -35,21 +37,21 @@ func (l *GetAvailableDaysLogic) GetAvailableDays(Type string) (resp *types.Avail
 	resp = &types.AvailableDatesRes{
 		Base: response.Success(),
 		Data: types.AvailableDates{
-			Dates: SyncAvaliableday(Type, dates),
+			Dates: SyncAvaliableday(Type, dates, debug),
 		},
 	}
 	return resp, nil
 }
 
 // 整合日期
-func SyncAvaliableday(Type string, dates []time.Time) []types.DateInfo {
+func SyncAvaliableday(Type string, dates []time.Time, debug any) []types.DateInfo {
 	if len(dates) == 0 {
 		return []types.DateInfo{}
 	}
 
 	var AvailableDates []types.DateInfo
 
-	if Type == "week" {
+	if Type == "week" && (canWeekReserve() || debug == "1") {
 		// 先筛选满足“周一”且后面还有完整一周的日期
 		// 先去除最后一天不是周日时，去掉最后一个周一
 		lastDate := dates[len(dates)-1]
@@ -65,7 +67,7 @@ func SyncAvaliableday(Type string, dates []time.Time) []types.DateInfo {
 		}
 
 		for i := 0; i < len(dates); i++ {
-			if dates[i].Weekday() == time.Monday {
+			if dates[i].Weekday() == time.Monday && dates[i].After(time.Now()) {
 				// 判断后面是否还有完整一周
 				weekEnd := dates[i].AddDate(0, 0, 6) // 这一周的周日
 				if !weekEnd.After(lastDate) {
@@ -75,10 +77,12 @@ func SyncAvaliableday(Type string, dates []time.Time) []types.DateInfo {
 						Date: dateStr,
 					})
 				}
+				// 只留一个
+				break
 			}
 		}
 
-	} else if Type == "day" { // day类型，只返回本周剩余天（不含今天），若今天是周日不返回
+	} else if Type == "day" && (canDayReserve() || debug == "1") { // day类型，只返回本周剩余天（不含今天），若今天是周日不返回
 		now := time.Now()
 		todayWeekday := now.Weekday()
 		if todayWeekday == time.Sunday {
@@ -97,4 +101,35 @@ func SyncAvaliableday(Type string, dates []time.Time) []types.DateInfo {
 		}
 	}
 	return AvailableDates
+}
+
+func canWeekReserve() bool {
+	now := time.Now()
+
+	// 判断是否为周日
+	if now.Weekday() != time.Sunday {
+		return false
+	}
+
+	// 获取当前小时和分钟
+	hour := now.Hour()
+	minute := now.Minute()
+
+	// 判断是否在 9:00 ~ 18:00 之间
+	if hour < 9 || (hour == 18 && minute > 0) || hour > 18 {
+		return false
+	}
+
+	return true
+}
+
+func canDayReserve() bool {
+	now := time.Now()
+
+	// 构造今天的 18:00 和 21:00
+	start := time.Date(now.Year(), now.Month(), now.Day(), 18, 0, 0, 0, now.Location())
+	end := time.Date(now.Year(), now.Month(), now.Day(), 21, 0, 0, 0, now.Location())
+
+	// 判断当前时间是否在区间内
+	return now.After(start) && now.Before(end)
 }
